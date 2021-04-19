1f1f206bec30116679e7d33d149e1047794d469d
Ra-Workspace <ra.workspace@gmail.com>
2021-04-19 10:49:48 +0900

    add NpcSpawn2 struct

--- src/common/Network/PacketDef/Zone/ServerZoneDef.h
+++ src/common/Network/PacketDef/Zone/ServerZoneDef.h
@@ -858,6 +858,82 @@ namespace Sapphire::Network::Packets::Server
     uint32_t unk34;
   };
 
+  struct FFXIVIpcNpcSpawn2 : FFXIVIpcBasePacket< NpcSpawn2 >
+  {
+    uint32_t gimmickId; // needs to be existing in the map, mob will snap to it
+    uint8_t u2b;
+    uint8_t u2ab;
+    uint8_t gmRank;
+    uint8_t u3b;
+
+    uint8_t aggressionMode; // 1 passive, 2 aggressive
+    uint8_t onlineStatus;
+    uint8_t u3c;
+    uint8_t pose;
+
+    uint32_t u4;
+
+    uint64_t targetId;
+    uint32_t u6;
+    uint32_t u7;
+    uint64_t mainWeaponModel;
+    uint64_t secWeaponModel;
+    uint64_t craftToolModel;
+
+    uint32_t u14;
+    uint32_t u15;
+    uint32_t bNPCBase;
+    uint32_t bNPCName;
+    uint32_t levelId;
+    uint32_t u19;
+    uint32_t directorId;
+    uint32_t spawnerId;
+    uint32_t parentActorId;
+    uint32_t hPMax;
+    uint32_t hPCurr;
+    uint32_t displayFlags;
+    uint16_t fateID;
+    uint16_t mPCurr;
+    uint16_t unknown1; // 0
+    uint16_t unknown2; // 0 or pretty big numbers > 30000
+    uint16_t modelChara;
+    uint16_t rotation;
+    uint16_t activeMinion;
+    uint8_t spawnIndex;
+    uint8_t state;
+    uint8_t persistantEmote;
+    uint8_t modelType;
+    uint8_t subtype;
+    uint8_t voice;
+    uint16_t u25c;
+    uint8_t enemyType;
+    uint8_t level;
+    uint8_t classJob;
+    uint8_t u26d;
+    uint16_t u27a;
+    uint8_t currentMount;
+    uint8_t mountHead;
+    uint8_t mountBody;
+    uint8_t mountFeet;
+    uint8_t mountColor;
+    uint8_t scale;
+    uint16_t elementalLevel; // Eureka
+    uint16_t element; // Eureka
+    Common::StatusEffect effect[30];
+    Common::FFXIVARR_POSITION3 pos;
+    uint32_t models[10];
+    char name[32];
+    uint8_t look[26];
+    char fcTag[6];
+    uint32_t unk30;
+    uint32_t unk31;
+    uint8_t bNPCPartSlot;
+    uint8_t unk32;
+    uint16_t unk33;
+    uint32_t unk34;
+    uint64_t unk35[47];
+  };
+
   /**
   * Structural representation of the packet sent by the server
   * to show player movement
